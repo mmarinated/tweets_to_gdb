c11bfcfd1c2daf92e18596ab63188d01b8465553
mmarinated <marina.a.zavalina@gmail.com>
2020-03-05 16:03:51 -0500

    Changed path

--- paths.py
+++ paths.py
@@ -1,4 +1,4 @@
 
-PATH_TO_REPO   = "/home/marina/python/interviews/ctrl/twitter/tweets_to_gdb"
+PATH_TO_REPO   = "/home/marina/python/interviews/ctrl/tweets_to_gdb"
 PATH_TO_JSON   = f"{PATH_TO_REPO}/etc/TwitterProject.json"
 PATH_TO_CONFIG = f"{PATH_TO_REPO}/etc/config.ini"
